5ecfce620d45cabab34f1a3f6eb0eda5fafb75bd
Daniel Park <37126844+DanPark13@users.noreply.github.com>
2019-12-29 21:48:05 -0500

    Upload Console 15

---
+++ ConsoleApplication15/ConsoleApplication15/Source.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	while (true)
+	{
+		int playerChoice;
+		cout << "Choose an attack 1-3:" << endl;
+		cout << "1) Sword Smash" << endl;
+		cout << "2) Fire Exploder" << endl;
+		cout << "3) Sneaky Dagger" << endl;
+
+		cin >> playerChoice;
+
+		switch (playerChoice)
+		{
+		case 1:
+			cout << " You swing away with your sword!" << endl;
+			break;
+		case 2:
+			cout << "You shoot a ton of fire out of your hands!" << endl;
+			break;
+		case 3:
+			cout << "You poke at the enemy with your dagger!" << endl;
+			break;
+		default:
+			cout << "You trip over your own feet and skip your turn!" << endl;
+		}
+		int enemyChoice = rand() % 3;
+
+		switch (enemyChoice)
+		{
+		case 0:
+			cout << "Your enemy runs at you with his sword" << endl;
+			break;
+		case 1:
+			cout << "Your enemy shoots fireballs at you" << endl;
+			break;
+		case 2:
+			cout << "Your enemy disappears, only to reappear next to you holding a dagger" << endl;
+			break;
+		}
+	}
+}
+
+	
+
